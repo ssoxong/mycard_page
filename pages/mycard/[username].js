ee6a44ef6876a7b3255779f62e42851999252393
ssoxong <ssoxong@naver.com>
2023-06-06 11:47:13 +0900

    dlsf

--- pages/mycard/[username].js
+++ pages/mycard/[username].js
@@ -2,26 +2,26 @@ import fetch from 'isomorphic-unfetch';
 import { useRouter } from 'next/router';
 import '../../styles/global.css';
 
-const fetchGitHubUser = async (accessToken) => { //access token 기반으로 username불러오기
-  try {
-    const response = await fetch('https://api.github.com/user', {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
+// const fetchGitHubUser = async (accessToken) => { //access token 기반으로 username불러오기
+//   try {
+//     const response = await fetch('https://api.github.com/user', {
+//       headers: {
+//         Authorization: `Bearer ${accessToken}`,
+//       },
+//     });
 
-    if (response.ok) {
-      const data = await response.json();
-      const username = data.login;
-      return username;
-    } else {
-      throw new Error('Failed to fetch GitHub user data');
-    }
-  } catch (error) {
-    console.error('Failed to fetch GitHub user data:', error);
-    // 오류 처리 로직
-  }
-};
+//     if (response.ok) {
+//       const data = await response.json();
+//       const username = data.login;
+//       return username;
+//     } else {
+//       throw new Error('Failed to fetch GitHub user data');
+//     }
+//   } catch (error) {
+//     console.error('Failed to fetch GitHub user data:', error);
+//     // 오류 처리 로직
+//   }
+// };
 // export async function getStaticPaths() {
 //   // 여기에서 필요한 로직을 추가하여 사용자 목록을 가져올 수 있습니다.
 //   // 예시로 두 명의 사용자를 가정합니다.
@@ -57,12 +57,10 @@ export async function getStaticProps({params}) {
       }
   };
 }
-const UserPage = ({  user_profile_info, repos, orgs }) => {
 
-  console.log("function in : ", user_profile_info)
+const UserPage = ({  user_profile_info, repos, orgs }) => {
   const router = useRouter();
 
-  //console.log('GitHub Username:', user.login);
   if (router.isFallback) {
     return <div>Loading...</div>;
   }
@@ -74,11 +72,8 @@ const UserPage = ({  user_profile_info, repos, orgs }) => {
       element.style.transform = "rotateY(180deg)";
     }
   }
-  //로그인 된 경우 Login 버튼 없애기
   return (
-      <> {
-      } {
-      } < section>
+      <>  < section>
         <div className="container mx-auto flex flex-col">
           <div onClick={onClick} className="card rounded-md w-96 h-60 bg-my-color">
               <div className="front">
@@ -145,13 +140,16 @@ const UserPage = ({  user_profile_info, repos, orgs }) => {
 
               <div className="back">
               <div className="flex flex-col items-center justify-center h-full">
-                  <div className="flex-shrink-0 mt-0 mb-0 p-0">
-                    <img
-                      src={`https://github-readme-stats.vercel.app/api?username=${user_profile_info.login}&show_icons=true&theme=tokyonight`}
-                      className="h-40"
-                    />
+                  <div className="flex-shrink-0 mt-0 mb-0 p-0" >
+                  <img    
+
+                    src={`https://github-readme-stats.vercel.app/api?username=${user_profile_info.login}&show_icons=true&theme=tokyonight`}
+                    className="h-40"
+
+/>
+
                   </div>
-                  <div className="flex-shrink-0">
+                  <div className="flex-shrink-0" >
                     <img
                       src={`https://github-readme-stats.vercel.app/api/top-langs/?username=${user_profile_info.login}&hide_progress=true&theme=tokyonight`}
                       className="h-20"
